b09fbfd8b66528397cb4adc2af5a42004fec46bf
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-04-04 13:50:15 -0400

    commit tree eed80117e13c2d257c53593ce454b913cff0770b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown num
+    while num > 0
+        num -= 1
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+    end
+   puts "HAPPY NEW YEAR!"
 end
+
